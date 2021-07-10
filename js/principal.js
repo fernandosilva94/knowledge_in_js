cdba7bc02ba4db83599634ee730cfe06599b7424
Carlos Duarte <cebduarte@outlook.com>
2021-07-10 01:02:12 -0300

    arq_add

--- js/principal.js
+++ js/principal.js
@@ -1,2 +1,8 @@
 var titulo = document.querySelector("h1");
 titulo.textContent = "Nutrição";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+
+console.log(paciente);
+console.log(tdPeso);
