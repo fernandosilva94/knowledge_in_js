7078a85c64b13454876f5f24fdb9dba78f7f9735
Fernando Silva <fernandoadmti@hotmail.com>
2021-07-18 23:36:33 -0300

    calculating_imc_for_all

--- js/principal.js
+++ js/principal.js
@@ -1,35 +1,43 @@
 /*var titulo = document.querySelector("h1");
 titulo.textContent = "Nutrição";
 */
-var paciente = document.querySelector("#primeiro-paciente");
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+for (var varre = 0; varre < pacientes.length; varre++) {
+    
+    var paciente = pacientes[varre];
+   
+    var tdPeso = paciente.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+    var tdAltura = paciente.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+    var tdImc = paciente.querySelector(".info-imc");
 
-var PesoValido = true;
-var AlturaValida = true;
+    var PesoValido = true;
+    var AlturaValida = true;
 
-if (peso <= 0 || peso >= 500) {
-    alert("Peso inválido");
-    PesoValido = false;
-    tdImc.textContent = "Peso inválido.";
+    if (peso <= 0 || peso >= 500) {
+        alert("Peso inválido");
+        PesoValido = false;
+        tdImc.textContent = "Peso inválido.";
+    }
+
+    if (altura <= 0 || altura >= 3) {
+        alert("Altura inválida");
+        AlturaValida = false;
+        tdImc.textContent = "Altura inválida.";
+    }
+
+    if (PesoValido && AlturaValida) {
+        var imc = peso / (altura * altura);
+    tdImc.textContent = imc.toFixed(2);
+    }
+
 }
 
-if (altura <= 0 || altura >= 3) {
-    alert("Altura inválida");
-    AlturaValida = false;
-    tdImc.textContent = "Altura inválida.";
-}
 
-if (PesoValido && AlturaValida) {
-    var imc = peso / (altura * altura);
-tdImc.textContent = imc;
-}
 
 /* 
 console.log(paciente);
