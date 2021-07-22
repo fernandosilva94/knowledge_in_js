91a78d7efddc7d37c7508f01188e9bd0f9fb0712
Fernando Silva <fernandoadmti@hotmail.com>
2021-07-22 09:29:03 -0300

    add_function_imc

--- js/form.js
+++ js/form.js
@@ -19,13 +19,13 @@ botaoAdicionar.addEventListener("click", function(event) {
     pesoTd.textContent = peso;
     alturaTd.textContent = altura;
     gorduraTd.textContent = gordura;
-    imcTd.textContent = imc;
+    imcTd.textContent = calculaImc(peso, altura);
 
     pacienteTr.appendChild(nomeTd);
     pacienteTr.appendChild(pesoTd);
     pacienteTr.appendChild(alturaTd);
     pacienteTr.appendChild(gorduraTd);
-    //pacienteTr.appendChild(imcTd);
+    pacienteTr.appendChild(imcTd);
 
     var tabela = document.querySelector("#tabela-pacientes");
     tabela.appendChild(pacienteTr);
